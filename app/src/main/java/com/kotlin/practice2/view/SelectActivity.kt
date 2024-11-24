fe97d2981ccbb5d561c6bc3c93148422d234aeda
jjy015 <wodbsabcd+1@gmail.com>
2024-11-24 21:43:57 +0900

    스택 제거를 통한 원활한 앱 종료 환경 개선

--- app/src/main/java/com/kotlin/practice2/view/SelectActivity.kt
+++ app/src/main/java/com/kotlin/practice2/view/SelectActivity.kt
@@ -41,14 +41,17 @@ class SelectActivity : AppCompatActivity() {
             viewModel.saveSelectedCoinList(selectRVAdapter.selectedCoinList)
         }
 
-        viewModel.saved.observe(this){
-            if(it.equals("done")){
-                val intent = Intent(this, MainActivity::class.java)
+        viewModel.saved.observe(this) {
+            if (it.equals("done")) {
+                val intent = Intent(this, MainActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                }
                 startActivity(intent)
 
                 saveInterestCoinDataPeriodic()
             }
         }
+
 
     }
 
